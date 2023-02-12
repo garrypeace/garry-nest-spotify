bf3663614744d45e548d32e5f9110f7be2a11b3e
Garry Peace <garrypeace@gmail.com>
2023-02-12 16:44:16 +0000

    Tidy up AppModule providers, these are unnecessary as the services are being provided in other imported modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ArtistsModule } from './artist/artists.module';
-import { SpotifyService } from './spotify/spotify.service';
 import { HttpModule } from '@nestjs/axios';
 import { PrismaModule } from './prisma/prisma.module';
-import { ArtistsCacheService as ArtistsCacheService } from './cache/artists-cache.service';
-import { ArtistsService } from './artist/artists.service';
 
 @Module({
   imports: [ArtistsModule, HttpModule, PrismaModule],
   controllers: [AppController],
-  providers: [SpotifyService, ArtistsCacheService, ArtistsService],
+  providers: [],
 })
 export class AppModule {}
