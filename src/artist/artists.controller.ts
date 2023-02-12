a12d92bade3ac0a299ebda979cdde5b3227dbc92
Garry Peace <garrypeace@gmail.com>
2023-02-12 16:41:42 +0000

    Modifications: - Flesh out rest of Controller endpoints. - Modify findOne() to throw exception if not found with the given ID. - Add TODOs

--- src/artist/artists.controller.ts
+++ src/artist/artists.controller.ts
@@ -1,15 +1,27 @@
 import {
+  Body,
   Controller,
+  Delete,
   Get,
+  NotFoundException,
   Param,
+  Patch,
+  Post,
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
 import { ArtistsService } from './artists.service';
 import { ArtistsCacheInterceptor } from 'src/cache/artists-cache.interceptor';
-import { ApiBadRequestResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ArtistEntity } from './entities/artist.entity';
 import { PrismaClientExceptionFilter } from 'src/prisma-client-exception/prisma-client-exception.filter';
+import { UpdateArtistDto } from './dto/update-artist.dto';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
 @UseFilters(PrismaClientExceptionFilter)
 @Controller('artists')
@@ -17,8 +29,13 @@ import { PrismaClientExceptionFilter } from 'src/prisma-client-exception/prisma-
 export class ArtistsController {
   constructor(private readonly artistsService: ArtistsService) {}
 
-  // this has to be above the findOne
-  @Get('random')
+  @Post()
+  @ApiCreatedResponse({ type: ArtistEntity })
+  create(@Body() createArtistDto: CreateArtistDto) {
+    return this.artistsService.create(createArtistDto);
+  }
+
+  @Get('random') // this method has to be above findOne() because of the param matching
   @ApiOkResponse({ type: ArtistEntity })
   findRandom() {
     return this.artistsService.findRandom();
@@ -28,13 +45,32 @@ export class ArtistsController {
   @ApiOkResponse({ type: ArtistEntity })
   @ApiBadRequestResponse()
   @UseInterceptors(ArtistsCacheInterceptor)
-  findOne(@Param('id') id: string) {
-    return this.artistsService.findOne(id);
+  async findOne(@Param('id') id: string) {
+    const article = await this.artistsService.findOne(id);
+
+    if (!article) {
+      throw new NotFoundException(`Could not find artist with ${id}.`);
+    }
+
+    return article;
   }
 
   @Get()
   @ApiOkResponse({ type: ArtistEntity, isArray: true })
   findAll() {
     return this.artistsService.findAll();
+  }
+
+  @Patch(':id')
+  @ApiCreatedResponse({ type: ArtistEntity })
+  update(@Param('id') id: string, @Body() updateArtistDto: UpdateArtistDto) {
+    return this.artistsService.update(id, updateArtistDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: ArtistEntity })
+  remove(@Param('id') id: string) {
+    // TODO: how should we handle deleting record that doesn't exist? 404/204?
+    return this.artistsService.remove(id);
   }
 }
